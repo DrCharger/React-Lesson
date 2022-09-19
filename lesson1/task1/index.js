9f921466f3e66a9cfdad44b373d9b622536f3691
DrCharger <dmitriy.nasa@gmail.com>
2022-09-19 11:27:28 +0300

    jt

--- lesson1/task1/index.js
+++ lesson1/task1/index.js
@@ -1,2 +1,6 @@
 /* eslint-disable no-undef */
+const rootElem = document.querySelector('#root');
 
+const greeting = React.createElement('div', { className: 'greeting' }, 'Hello, React!');
+
+ReactDOM.render(greeting, rootElem);
