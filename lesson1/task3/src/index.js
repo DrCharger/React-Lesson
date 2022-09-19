a12165232fba1cab3e303960a38e80952768edcc
DrCharger <dmitriy.nasa@gmail.com>
2022-09-19 12:26:26 +0300

    ght2

---
+++ lesson1/task3/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const rootElement = document.querySelector('#root');
+
+const elem = (
+  <div class="greeting">
+    <div class="greeting__title">Hello, world!</div>
+    <div class="greeting__text">I'am learning React</div>
+  </div>
+);
+
+ReactDOM.render(elem, rootElement);
+
+// renderGreeting(rootElement);
