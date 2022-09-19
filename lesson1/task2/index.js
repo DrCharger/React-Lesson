66283fae6253d991a20cf1d705237a217cde848a
DrCharger <dmitriy.nasa@gmail.com>
2022-09-19 11:42:13 +0300

    k;'

--- lesson1/task2/index.js
+++ lesson1/task2/index.js
@@ -21,7 +21,7 @@ const el = React.createElement(
   'div',
   { className: 'greeting' },
   React.createElement('h1', { className: 'greeting__title' }, 'Hello, world!'),
-  React.createElement('p', { className: 'greeting__Text' }, "I'm learning React"),
+  React.createElement('p', { className: 'greeting__text' }, "I'm learning React"),
 );
 
 ReactDOM.render(el, rootElement);
