15a5740caba5b1d73d0f382605885ea9c2fd23de
DrCharger <dmitriy.nasa@gmail.com>
2022-09-21 13:57:40 +0300

    qwr

---
+++ lesson3/task3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Comment from './Comment';
+
+const UserInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com/avatar',
+};
+
+const App = () => {
+  return <Comment user={UserInfo} text="Good job!" date={new Date()} />;
+};
+export default App;
