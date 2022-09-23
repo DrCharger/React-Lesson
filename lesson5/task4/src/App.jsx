9fe2527c99eb16682047cf27d41ccbdb5cffd02d
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-23 14:52:56 +0300

    lkjhgf

---
+++ lesson5/task4/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Colors from './Colors.jsx';
+import './index.scss';
+
+const App = () => {
+  return (
+    <>
+      <Colors />
+    </>
+  );
+};
+
+export default App;
