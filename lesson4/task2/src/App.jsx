27fc60f769bc087bf71fcb27898d5e946e4b0157
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-23 12:47:04 +0300

    sfg

---
+++ lesson4/task2/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Clock from './Clock.jsx';
+import './index.scss';
+
+const App = () => {
+  return (
+    <>
+      <Clock location="New York" offset={-5} />
+      <Clock location="Kyiv" offset={3} />
+      <Clock location="London" offset={0} />
+    </>
+  );
+};
+
+export default App;
---
+++ lesson4/task2/src/Clock.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './clock.scss';
+import moment from 'moment';
+
+const formatDate = date => moment(date).format('LTS');
+
+const getTimeWithOffset = offset => {
+  const currentTime = new Date();
+  const utcOffset = currentTime.getTimezoneOffset() / 60;
+  return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
+};
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: props.location,
+    };
+    setInterval(() => {
+      this.setState({
+        time: getTimeWithOffset(props.offset),
+      });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="clock">
+        <div className="clock__location">{`${this.state.location}`}</div>
+        <div className="clock__time">{`${formatDate(this.state.time)}`}</div>
+      </div>
+    );
+  }
+}
+export default Clock;
