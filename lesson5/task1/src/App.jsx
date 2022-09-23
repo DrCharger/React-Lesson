75a53bd6e4b1939e312d5b16042c1d941eed03ca
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-23 13:40:45 +0300

    fg

---
+++ lesson5/task1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GoodButton from './GoodButton.jsx';
+import './index.scss';
+
+const App = () => {
+  return (
+    <>
+      <GoodButton />
+    </>
+  );
+};
+
+export default App;
---
+++ lesson5/task1/src/GoodButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GoodButton = () => {
+  return (
+    <button onClick={() => alert('Good job!')} className="fancy-button">
+      Click me!
+    </button>
+  );
+};
+
+export default GoodButton;
