23af3cdb8965dba9511e37a30132c7480a7359a0
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-23 15:28:57 +0300

    j

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -13,18 +13,18 @@ class ColorPicker extends React.Component {
         <div className="picker__title"></div>
         <div>
           <button
-            onMouseEnter={() => this.picker('Coral')}
-            onMouseLeave={this.reset}
+            onMouseOver={() => this.picker('Coral')}
+            onMouseOut={this.reset}
             className="picker__button picker__button_coral"
           ></button>
           <button
-            onMouseEnter={() => this.picker('Aqua')}
-            onMouseLeave={this.reset}
+            onMouseOver={() => this.picker('Aqua')}
+            onMouseOut={this.reset}
             className="picker__button picker__button_aqua"
           ></button>
           <button
-            onMouseEnter={() => this.picker('Bisque')}
-            onMouseLeave={this.reset}
+            onMouseOver={() => this.picker('Bisque')}
+            onMouseOut={this.reset}
             className="picker__button picker__button_bisque"
           ></button>
         </div>
