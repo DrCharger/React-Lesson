ed1436ca85b40f2f462e43f44e96b349e31a9b95
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-26 15:34:49 +0300

    saasas

--- lesson6/task4/src/Status.jsx
+++ lesson6/task4/src/Status.jsx
@@ -6,35 +6,10 @@ import './index.scss';
 class Status extends React.Component {
   state = {
     isOnline: true,
-    isOffline: true,
-  };
-  online = () => {
-    this.setState({
-      isOnline: false,
-      isOffline: false,
-    });
-  };
-  offline = () => {
-    this.setState({
-      isOnline: true,
-      isOffline: true,
-    });
   };
 
   render() {
-    // let status;
-    // if (this.state.isOnline) {
-    //   status = <Online ok={() => this.online()} />;
-    // }
-    // if (!this.state.isOffline) {
-    //   status = <Offline ok={() => this.offline()} />;
-    // }
-    return (
-      <div className="status">
-        {this.state.isOnline && <Online />}
-        {!this.state.isOffline && <Offline />}
-      </div>
-    );
+    return <div className="status">{this.state.isOnline ? <Online /> : <Offline />}</div>;
   }
 }
 
