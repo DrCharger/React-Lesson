dbe38474ae2d076594933e7cda55533417326567
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-27 11:54:02 +0300

    kjgf

--- lesson7/task1/src/index.jsx
+++ lesson7/task1/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import NumberList from './NumberList';
+import NumberList from './NumbersList';
 
 const rootElem = document.querySelector('#root');
 const numbers = [1, 2, 3, 4, 5];
