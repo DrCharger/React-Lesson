9906b6064b54265e3cef5f0e64167082681bedd3
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-28 13:09:18 +0300

    dfg

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -4,6 +4,9 @@ class Life extends Component {
   constructor(props) {
     super(props);
     console.log('constructor: good place to create state');
+    this.state = {
+      a: null,
+    };
   }
 
   componentDidMount() {
@@ -14,7 +17,7 @@ class Life extends Component {
     console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
   }
 
-  componentDidUpdate() {
+  componentDidUpdate(prevProps, prevState) {
     console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
   }
 
