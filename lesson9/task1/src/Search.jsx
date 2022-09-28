eda77a9f5c96bdfe9b977f7d6ca8e2cabf700487
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-28 16:32:30 +0300

    dfrg

---
+++ lesson9/task1/src/Search.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  state = {
+    value: '',
+  };
+  handdleChange = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+  handdleSubmit = event => {
+    event.preventDefault();
+    alert(`Search text: ${this.state.value}`);
+  };
+  render() {
+    return (
+      <form class="search" onSubmit={this.handdleSubmit}>
+        <input
+          type="text"
+          onChange={this.handdleChange}
+          className="search__input"
+          value={this.state.value}
+          name="name"
+        />
+        <button class="search__button" type="submit">
+          Search
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Search;
