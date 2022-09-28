75a4bf0945afc4e90b695f3a343229fe73bafc91
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-09-28 14:18:16 +0300

    hgh

--- lesson8/task3/src/index.jsx
+++ lesson8/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Dimentions from './Dimentions.jsx';
+import Dimensions from './Dimensions.jsx';
 import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Dimentions />, rootElement);
+ReactDOM.render(<Dimensions />, rootElement);
