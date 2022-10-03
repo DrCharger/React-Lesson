e381d6c88104f0dfeba2b7366a8ac8114fa52ce5
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-03 13:53:09 +0300

    asd

--- lesson11/task1/src/Numbers.jsx
+++ lesson11/task1/src/Numbers.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-class Numbers extends Component {
+class Numbers extends PureComponent {
   render() {
     return (
       <div className="number">
