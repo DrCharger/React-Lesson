63cac17e2ef20d7a9433c962dda6b929fa52f240
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-03 21:31:11 +0300

    gh

--- lesson11/task3/src/Expand.jsx
+++ lesson11/task3/src/Expand.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Expand = ({ children, title, isOpen, handleChange }) => {
-  let icon;
-  if (!isOpen) {
-    icon = <i className="fas fa-chevron-down"></i>;
-  } else {
-    icon = <i className="fas fa-chevron-up"></i>;
+class Expand extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
   }
-  return (
-    <div className="expand border">
-      <div className="expand__header">
-        <span className="expand__title">{title}</span>
-        <button className="expand__toggle-btn" onClick={handleChange}>
-          {icon}
-        </button>
+
+  handleChange = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    let icon;
+    let content;
+    const { children, title } = this.props;
+    if (!this.state.isOpen) {
+      icon = <i className="fas fa-chevron-down"></i>;
+    } else {
+      icon = <i className="fas fa-chevron-up"></i>;
+      content = children;
+    }
+    return (
+      <div className="expand border">
+        <div className="expand__header">
+          <span className="expand__title">{title}</span>
+          <button className="expand__toggle-btn" onClick={this.handleChange}>
+            {icon}
+          </button>
+        </div>
+        <div className="expand__content">{content}</div>
       </div>
-      <div className="expand__content">{isOpen && children}</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Expand;
