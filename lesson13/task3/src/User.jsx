cacc0d29be24ee96c1ef7296ddcb78f6ef40c167
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-05 21:24:03 +0300

    kjghkj

--- lesson13/task3/src/User.jsx
+++ lesson13/task3/src/User.jsx
@@ -1,36 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const User = () => {
-  const [fulldata, setData] = useState(null);
-
+  const [userData, setData] = useState(null);
   const { productId } = useParams();
-
-  const fetchDetails = () => {
+  console.log(productId);
+  const fetchUser = () => {
     fetch(`http://api.github.com/users/${productId}`)
       .then(response => response.json())
       .then(data => setData(data));
   };
-
   useEffect(() => {
-    fetchDetails();
-  });
-
-  if (fulldata === null) {
+    fetchUser();
+  }, []);
+  if (userData === null) {
     return null;
-  } else {
-    const { avatar_url, name, location } = fulldata;
-
-    return (
-      <div className="user">
-        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
-        <div className="user__info">
-          <span className="user__name">{name}</span>
-          <span className="user__location">{location}</span>
-        </div>
-      </div>
-    );
   }
+  console.log(userData.name);
+  const { avatar_url, name, location } = userData;
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
 };
 
 export default User;
