23c38c3a8ce757d799fbc9e39dfb59ee16bba5fe
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-05 14:46:29 +0300

    hjgf

---
+++ lesson13/task1/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Contacts from './Contacts';
+import Products from './Products';
+import PageNotFound from './PageNotFound';
+
+const App = () => {
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {' '}
+            <Home />
+          </Route>
+          <Route path="/contacts">
+            {' '}
+            <Contacts />
+          </Route>
+          <Route path="/products">
+            {' '}
+            <Products />
+          </Route>
+          <Route>
+            {' '}
+            <PageNotFound path="*" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
