5485cbf22cefd0bd9674fc57edcaf86c0ae3711d
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-05 22:00:53 +0300

    lkjhjkh

--- lesson13/task3/src/User.jsx
+++ lesson13/task3/src/User.jsx
@@ -4,24 +4,17 @@ import { useParams } from 'react-router-dom';
 const User = () => {
   const { productId } = useParams();
   const [userData, setData] = useState(null);
-  const [user, setUser] = useState(productId);
 
   const fetchUser = () => {
     fetch(`http://api.github.com/users/${productId}`)
       .then(response => response.json())
       .then(data => setData(data));
   };
-  useEffect(() => {
-    fetchUser();
-  }, []);
-  if (user !== productId) {
-    fetchUser();
-    setUser(productId);
-  }
+  fetchUser();
+
   if (userData === null) {
     return null;
   }
-  console.log(userData.name);
   const { avatar_url, name, location } = userData;
   return (
     <div className="user">
