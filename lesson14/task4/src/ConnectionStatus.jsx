c9bda633c9bc3b241c436e5c520bff315023468f
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-06 14:05:48 +0300

    jutyde

--- lesson14/task4/src/ConnectionStatus.jsx
+++ lesson14/task4/src/ConnectionStatus.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 const ConnectionStatus = () => {
-  const [status, setStatus] = useState(true);
+  const [isOnline, setOnline] = useState(true);
+
   const toggleOnline = () => {
-    setStatus(!status);
+    setOnline(!isOnline);
   };
 
   useEffect(() => {
@@ -16,8 +17,8 @@ const ConnectionStatus = () => {
     };
   });
 
-  return status ? (
-    <div className="status ">Online</div>
+  return isOnline ? (
+    <div className="status">Online</div>
   ) : (
     <div className="status status_offline">Offline</div>
   );
