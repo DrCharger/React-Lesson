51562ee9469bb5cc93b9667e0220e6e34e06b46f
Dima Nasadiuk <dmitriy.nasa@gmail.com>
2022-10-06 12:36:00 +0300

    jhgd

---
+++ lesson14/task2/src/User.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const User = () => {
+  const { userId } = useParams();
+
+  const [userData, setData] = useState(null);
+
+  const fetchUser = () => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error();
+      })
+      .then(data => setData(data));
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, [userId]);
+  if (!userData) {
+    return null;
+  }
+
+  const { avatar_url, name, location } = userData;
+  return (
+    <div className="user">
+      <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+      <div className="user__info">
+        <span className="user__name">{name}</span>
+        <span className="user__location">{location}</span>
+      </div>
+    </div>
+  );
+};
+
+export default User;
